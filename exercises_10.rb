d5c485299b5c09bb887fcd4427422a2b2ee6636e
Kai-Min Chang <changkaimin.big@gmail.com>
2014-08-09 22:55:13 +0800

    2014/8/9_3

---
+++ exercises_10.rb
@@ -0,0 +1,6 @@
+#hash values be arrays
+hash = {movies: ["batman", "spiderman", "Thor"]}
+p hash
+#array of hashes
+array = [{movies: "batman"}, {movies: "spiderman"}, {movies: "Thor"}]
+p array
---
+++ exercises_11.rb
@@ -0,0 +1,2 @@
+#I like ruby-doc.org because the documentation 
+#is complete and easy to search and read.
