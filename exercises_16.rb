f8388109f03c195412c5b854e885021ea68dea17
kmc653 <kmc653@users.noreply.github.com>
2014-08-09 23:06:07 +0800

    Delete exercises_16.rb

--- exercises_16.rb
+++
@@ -1,9 +0,0 @@
-a = ['white snow', 'winter wonderland', 'melting ice',
-     'slippery sidewalk', 'salted roads', 'white trees']
-new_arr = []
-
-a.map { |word| new_arr.push(word.split)}
-
-new_arr.flatten!
-
-p new_arr
