655654f0dead280fe3bec4ab08dcdcfa1a46e6d8
Kevin Chang <changkaimin.big@gmail.com>
2014-08-09 13:48:24 +0800

    2014/8/9_2

---
+++ exercises_5.rb
@@ -0,0 +1,7 @@
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+number.push(11)
+number.unshift(0)
+
+number.pop
+number.push(3)
---
+++ exercises_8.rb
@@ -0,0 +1,5 @@
+hash_1 = { ruby: 1, java: 2, python: 3}
+hash_2 = { :ruby => 1, :java => 2, :python => 3}
+
+p hash_1
+p hash_2
